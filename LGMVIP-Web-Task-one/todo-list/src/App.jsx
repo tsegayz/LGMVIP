fa7eec62cd7ee10ce2f89c2ca789fb7d7a3bf609
Tsega Yaekob <tsegayaekob7@gmail.com>
2023-08-03 03:15:28 +0300

    route

--- LGMVIP-Web-Task-one/todo-list/src/App.jsx
+++ LGMVIP-Web-Task-one/todo-list/src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Routes instead of Switch
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Task from "./Task";
 import Home from "./Home";
 
 function App() {
-	return (
-		<div className='App'>
-			<Router>
-				<div className='content'>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/task' element={<Task />} />
-					</Routes>
-				</div>
-			</Router>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Router>
+        <div className='content'>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/task' component={Task} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
